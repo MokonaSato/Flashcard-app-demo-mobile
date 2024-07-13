65e000dce09962cf7204e77c4c6371ac46fac294
MokonaSato <mokonasato@MokonaSatonoMac-mini.local>
2024-07-14 00:31:40 +0900

    rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/レポジトリ名/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Flashcard-app-demo/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
