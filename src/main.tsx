97e20af6308633c4dc1d40600537a982e3e8c8e0
MokonaSato <mokonasato@MokonaSatonoMac-mini.local>
2024-07-14 14:41:51 +0900

    change url setting

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Flashcard-app-demo/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Flashcard-app-demo-mobile/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
